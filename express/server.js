14742e1464c38f82f04069d5093a082f7a77c3e2
Damy Metzke <damy@brightboost.nl>
2022-09-22 15:16:26 +0200

    feat: Add some stats

--- express/server.js
+++ express/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import _ from "lodash";
 
 const app = express();
 
@@ -12,10 +13,31 @@ app.use(express.urlencoded({ extended: true }));
 const tasks = [];
 let idIncrement = 0;
 
-app.get("/", (_, res) => {
-  res.render("index", {
-    list: tasks.sort(({ difficulty: a }, { difficulty: b }) => a - b)
-  });
+app.get("/", (__, res) => {
+  tasks.sort(({ difficulty: a }, { difficulty: b }) => a - b);
+  const activeTasks = tasks.filter(({ complete }) => !complete);
+
+  if (activeTasks.length === 0) {
+    res.render("index", {
+      list: tasks,
+      average: "-",
+      median: "-",
+    });
+  }
+  else {
+    const totalDifficulty = _.sum(activeTasks.map(({ difficulty }) => difficulty));
+
+    const median = activeTasks.length % 2 === 0
+      ? (activeTasks[activeTasks.length / 2 - 1].difficulty + activeTasks[activeTasks.length / 2].difficulty) / 2
+      : activeTasks[Math.floor(activeTasks.length / 2)].difficulty;
+
+    res.render("index", {
+      list: tasks.sort(({ difficulty: a }, { difficulty: b }) => a - b),
+      average: totalDifficulty / activeTasks.length,
+      median,
+    });
+  }
+
 })
 
 app.post("/", (req, res) => {
