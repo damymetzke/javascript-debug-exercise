e03443dedd45406bb48092dba5382a53aa590f16
Damy Metzke <damy@brightboost.nl>
2022-09-22 12:23:38 +0200

    Add support for completing tasks

--- express/server.js
+++ express/server.js
@@ -10,6 +10,7 @@ app.use(express.static(dirname(fileURLToPath(import.meta.url)) + '/public'))
 app.use(express.urlencoded({ extended: true }));
 
 const tasks = [];
+let idIncrement = 0;
 
 app.get("/", (_, res) => {
   res.render("index", {
@@ -19,7 +20,17 @@ app.get("/", (_, res) => {
 
 app.post("/", (req, res) => {
   const { description, difficulty } = req.body;
-  tasks.push({ description, difficulty });
+  tasks.push({ description, difficulty, complete: false, id: ++idIncrement });
+  res.redirect("/");
+})
+
+app.post("/:id", (req, res) => {
+  for (const task of tasks) {
+    if (task.id !== parseInt(req.params.id)) {
+      continue;
+    }
+    task.complete = !task.complete;
+  }
   res.redirect("/");
 })
 
