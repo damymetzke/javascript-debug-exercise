f57125253861cee017ca163a31379c59e6d4b32c
Damy Metzke <damy@brightboost.nl>
2022-09-22 11:55:57 +0200

    feat: Implement post route in express example

--- express/server.js
+++ express/server.js
@@ -1,17 +1,26 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
 
 app.set("view engine", "pug");
 
+app.use(express.static(dirname(fileURLToPath(import.meta.url)) + '/public'))
+app.use(express.urlencoded({ extended: true }));
+
+const tasks = [];
+
 app.get("/", (_, res) => {
   res.render("index", {
-    list: [
-      { description: "aaa", difficulty: 10 },
-      { description: "bbb", difficulty: 20 },
-      { description: "ccc", difficulty: 30 },
-      { description: "ddd", difficulty: 5 },
-    ].sort(({ difficulty: a }, { difficulty: b }) => a - b)
+    list: tasks.sort(({ difficulty: a }, { difficulty: b }) => a - b)
   });
+})
+
+app.post("/", (req, res) => {
+  const { description, difficulty } = req.body;
+  tasks.push({ description, difficulty });
+  res.redirect("/");
 })
 
 app.listen(3000);
