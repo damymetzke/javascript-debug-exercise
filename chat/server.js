220222d0fa5427166bbe6d3286f0281337fb10ac
Damy Metzke <damy@brightboost.nl>
2022-09-30 16:17:34 +0200

    feat: Add colors to chat based on name

--- chat/server.js
+++ chat/server.js
@@ -4,6 +4,25 @@ import { fileURLToPath } from "url";
 import _ from "lodash";
 import { createServer } from "http";
 import { WebSocketServer } from "ws";
+import { createHash } from "crypto";
+
+function getColorFromName(name) {
+  const hash = createHash("sha1");
+  hash.update(name);
+  const fullHash = hash.digest("hex");
+
+  return "#" + fullHash.substring(0, 6);
+}
+
+function getForegroundColorFromBackground(color) {
+	var r = parseInt(color.substring(1,3),16);
+	var g = parseInt(color.substring(3,5),16);
+	var b = parseInt(color.substring(5,7),16);
+
+	var yiq = ((r*299)+(g*587)+(b*114))/1000;
+
+	return (yiq >= 128) ? 'black' : 'white';
+}
 
 const app = express();
 const sockets = new Set();
@@ -20,13 +39,16 @@ app.get("/", (__, res) => {
 });
 
 app.post("/", req => {
+  const background = getColorFromName(req.body.name);
+  const foreground = getForegroundColorFromBackground(background);
+
   for (const socket of sockets) {
     socket.send(
       JSON.stringify({
         name: req.body.name,
         message: req.body.message,
-        background: "#604512",
-        foreground: "#fff",
+        background,
+        foreground,
       }),
     );
   }
